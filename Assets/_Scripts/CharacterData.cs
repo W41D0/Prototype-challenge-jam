bcbbff549998aed414d7a03e3f2a02f7107baaf8
TheMKDev <mns.alkhayat@gmail.com>
2026-09-12 13:24:24 +0300

    basic setup

---
+++ Assets/_Scripts/CharacterData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterData", menuName = "ScriptableObjects/CharacterData", order = 1)]
+public class CharacterData : ScriptableObject
+{
+    public string Name;
+}
